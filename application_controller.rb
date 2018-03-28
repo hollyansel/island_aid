19ea4cbbcdc377eec1d7a71e914406cf4db6ad24
Holly Ansel <hollya3643@gmail.com>
2018-03-28 14:13:03 +0000

    Ruby Files Added

---
+++ application_controller.rb
@@ -0,0 +1,19 @@
+require './config/environment'
+require 'pry' #pry is a VERY useful gem for debugging. By requiring it here, we can freeze at any point during our applicaiton by using binding.pry
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+  
+   get '/' do
+    erb :index
+  end
+  
+  get '/generic' do
+    erb :generic
+    end 
+  
+end  
